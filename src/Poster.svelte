78f28e417ec29e62c7e0bf67a3cf1929ffdc7f66
notwindstone <notwindstone@gmail.com>
2025-06-09 11:52:46 +0300

    feat: update styles

--- src/Poster.svelte
+++ src/Poster.svelte
@@ -1,1 +1,14 @@
 <img class="windose-extension-custom-poster" src="https://raw.githubusercontent.com/notwindstone/anisun-svelte-css-styles-extension/refs/heads/main/public/windose-logo.webp" alt="Windose Logo" />
+
+<style>
+.windose-extension-custom-poster {
+  position: absolute;
+  top: 64px;
+  left: calc(50% + 96px);
+  width: 224px;
+  
+  @media screen and (max-width: 640px) {
+    display: none;
+  }
+}
+</style>
